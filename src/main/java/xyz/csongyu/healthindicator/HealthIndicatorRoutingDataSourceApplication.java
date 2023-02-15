7a044ff4a76b4da0f2e7cbaa76e99de67e6fba70
songor <chensongyu1024@gmail.com>
2023-02-15 22:50:19 +0800

    exclude auto configuration

--- src/main/java/xyz/csongyu/healthindicator/HealthIndicatorRoutingDataSourceApplication.java
+++ src/main/java/xyz/csongyu/healthindicator/HealthIndicatorRoutingDataSourceApplication.java
@@ -2,8 +2,10 @@ package xyz.csongyu.healthindicator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, JdbcTemplateAutoConfiguration.class})
 public class HealthIndicatorRoutingDataSourceApplication {
     public static void main(final String[] args) {
         SpringApplication.run(HealthIndicatorRoutingDataSourceApplication.class, args);
